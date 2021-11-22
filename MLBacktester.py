371ba499712a42a151d910dadb0fca3083d7c3e5
Andrew Zang <andrewz020529@gmail.com>
2021-11-21 21:32:05 -0500

    start ml backtesting

---
+++ MLBacktester.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 12:25:24 2021
+
+@author: andrewzang
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
